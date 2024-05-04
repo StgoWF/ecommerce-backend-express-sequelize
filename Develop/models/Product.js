d6d345f245b85c8abc8536ccc966ffa946954e5f
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-05-04 08:20:42 -0400

    Define Product model with columns and validations

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,15 +1,45 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Extends off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Initialize Product model (table) by extending off Sequelize's Model class
 Product.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,  // Validates the price to ensure it is a decimal
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,  // Default stock value when none is provided
+      validate: {
+        isNumeric: true,  // Ensures the stock value is numeric
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',  // Establishes a reference to the Category model
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
