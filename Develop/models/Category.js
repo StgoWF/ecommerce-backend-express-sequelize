a4cb4d7fa4a789b71016f94edb535c966fb56f48
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-05-04 08:19:57 -0400

    Define Category model with columns and configuration

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// Extending the Model class from Sequelize
 class Category extends Model {}
 
+// Initializing Category model (table) by extending Sequelize's Model class
 Category.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,  // Automatically increment the ID for each new entry
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,  // This field cannot be empty
+    }
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    timestamps: false,  // No need to keep track of timestamps for this model
+    freezeTableName: true,  // Prevent Sequelize from renaming the table
+    underscored: true,  // Supports the use of underscores instead of camelCasing
+    modelName: 'category',  // This is how the model is identified in Sequelize
   }
 );
 
